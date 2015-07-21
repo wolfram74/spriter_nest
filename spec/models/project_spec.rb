4424a86bfb0df03819792fe62e62a2789f57dce5
wolfram74 <phaugen@gmail.com>
2015-07-21 09:52:59 -0700

    set up user associations tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it {should have_many(:auths)}
+    it {should have_many(:projects)}
+  end
 end
