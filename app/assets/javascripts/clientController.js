81c634411846784aa1b041ab5d9768064c29fcfb
wolfram74 <phaugen@gmail.com>
2015-07-21 11:30:37 -0700

    modularizing client side operations

---
+++ app/assets/javascripts/clientController.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  if(window.location.hash){
+    console.log("hash present")
+    clientController.hashParser(window.location.hash)
+  }else{ console.log("no hash present")}
+});
+
+clientController = (function(){
+  var API = {}
+  API.authState = {}
+  API.hashParser = function(string){
+    string = string.slice(1)
+    var terms = string.split("&")
+    for(var index in terms){
+      console.log(terms[index])
+      var pair = terms[index].split("=")
+      API.authState[pair[0]] = pair[1]
+    };
+    console.log(API.authState)
+  };
+  return API
+})()
