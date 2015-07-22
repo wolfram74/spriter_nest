1f06ef07d782a27a10fc0bad7ab27031527d8ab0
wolfram74 <phaugen@gmail.com>
2015-07-22 11:20:48 -0700

    har har har

--- spec/models/auth_spec.rb
+++ spec/models/auth_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Auth, type: :model do
   describe "associations" do
-    it {should belong_to(:jared)} # muhahahahaa!
+    it {should belong_to(:user)}
   end
 end
