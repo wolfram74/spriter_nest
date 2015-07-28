c5751374783d686caef9177c61545d356916c4cb
wolfram74 <phaugen@gmail.com>
2015-07-28 16:54:39 -0700

    wrote to_atlas tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ActiveRecord::Base
   has_many :slides
   belongs_to :user
+
+  def to_atlas
+    output = {}
+    self.slides.each{|slide| output[slide.title] = slide.to_atlas}
+    return output
+  end
 end
