53fbe8ed9d2855844050f18121aac75a4b42a246
wolfram74 <phaugen@gmail.com>
2015-08-10 10:10:44 -0700

    wrote several dom-related tests for pad object

--- spec/javascripts/pads_spec.js
+++ spec/javascripts/pads_spec.js
@@ -13,14 +13,37 @@ describe("pad properties", function(){
     it("has a virtual dom",function(){
       expect(pad.$dom.is("div")).toEqual(true)
     })
+
     it("virtual dom contains zoom selector",function(){
-      expect(pad.$dom.find("jsZoomSelect").is("div")).toEqual(true)
+      expect(pad.$dom.find("#jsZoomSelect").is("div")).toEqual(true)
     })
+    it("zoom selector contains a form",function(){
+      var zoom = pad.$dom.find("#jsZoomSelect")
+      expect(!!zoom.find("form").length).toEqual(true)
+    })
+
     it("virtual dom contains color selector",function(){
-      expect(pad.$dom.find("jsColorSelect").is("div")).toEqual(true)
+      expect(pad.$dom.find("#jsColorSelect").is("div")).toEqual(true)
     })
+    it("color selector contains a form",function(){
+      var color = pad.$dom.find("#jsColorSelect")
+      expect(!!zoom.find("form").length).toEqual(true)
+    })
+    it("color selector shows present color",function(){
+      var color = pad.$dom.find("#jsColorSelect")
+      expect(!!zoom.find("#jsCurrent color").length).toEqual(true)
+    })
+
     it("virtual dom contains slide selector",function(){
-      expect(pad.$dom.find("jsSlideSelect").is("div")).toEqual(true)
+      expect(pad.$dom.find("#jsSlideSelect").is("div")).toEqual(true)
+    })
+
+    it("virtual dom contains slide main stage",function(){
+      expect(pad.$dom.find("#jsSlideView").is("div")).toEqual(true)
+    })
+
+    it("virtual dom contains animation screen",function(){
+      expect(pad.$dom.find("#jsAnimation").is("div")).toEqual(true)
     })
   })
 })
